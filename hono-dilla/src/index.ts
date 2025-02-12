059568e8483f402da240cebe62f6dbad5dcb826f
Adilla <adilla@gmail.com>
2025-02-12 09:29:46 +0700

    add CORS middleware to enable cross-origin requests #2

--- hono-dilla/src/index.ts
+++ hono-dilla/src/index.ts
@@ -2,8 +2,19 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { Routes } from "./routes/index.js";
 import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
 
 const app = new Hono().basePath("/api");
+
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:3000",
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["*"],
+  })
+);
+
 const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
 
@@ -13,7 +24,7 @@ serve({
 });
 
 app.route("/posts", Routes);
-app.route("/adilla", Routes);
+// app.route("/adilla", Routes);
 export const GET = handle(app);
 export const POST = handle(app);
 export const PUT = handle(app);
